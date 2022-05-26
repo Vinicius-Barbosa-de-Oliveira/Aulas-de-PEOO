7f4c10f11454f82f39c538a48ca781ceebed9200
Vinicius Barbosa <94053437+Vinicius-Barbosa-de-Oliveira@users.noreply.github.com>
2022-05-26 01:20:24 +0000

    dezenas de testes que não servira de nada

--- Aula23-05/lista-rev-02/ex03.cs
+++ Aula23-05/lista-rev-02/ex03.cs
@@ -7,12 +7,57 @@ class Program {
 		int n3 = int.Parse(Console.ReadLine());
 		int n4 = int.Parse(Console.ReadLine());
 		
-		Console.WriteLine("Resultados");
+		Console.WriteLine("Resultados 01");
+		if (n1 % 2 == 1 && n2 % 2 == 1) Console.WriteLine("soma dos impares = " + (n1 + n2));
+		if (n1 % 2 == 1 && n3 % 2 == 1) Console.WriteLine("soma dos impares = " + (n1 + n3));
+		if (n1 % 2 == 1 && n4 % 2 == 1) Console.WriteLine("soma dos impares = " + (n1 + n4));
+		if (n2 % 2 == 1 && n3 % 2 == 1) Console.WriteLine("soma dos impares = " + (n2 + n3));
+		if (n2 % 2 == 1 && n4 % 2 == 1) Console.WriteLine("soma dos impares = " + (n2 + n4));
+		if (n3 % 2 == 1 && n4 % 2 == 1) Console.WriteLine("soma dos impares = " + (n3 + n4));
+		
+		if (n1 % 2 == 0 && n2 % 2 == 0) Console.WriteLine("soma dos pares = " + (n1 + n2));
+		if (n1 % 2 == 0 && n3 % 2 == 0) Console.WriteLine("soma dos pares = " + (n1 + n3));
+		if (n1 % 2 == 0 && n4 % 2 == 0) Console.WriteLine("soma dos pares = " + (n1 + n4));
+		if (n2 % 2 == 0 && n3 % 2 == 0) Console.WriteLine("soma dos pares = " + (n2 + n3));
+		if (n2 % 2 == 0 && n4 % 2 == 0) Console.WriteLine("soma dos pares = " + (n2 + n4));
+		if (n3 % 2 == 0 && n4 % 2 == 0) Console.WriteLine("soma dos pares = " + (n3 + n4));
+		Console.WriteLine("Resultados 02 = fracasso");
+		/*if ((n1 + n2 + n3 + n4) % 2 == 0) Console.WriteLine(n1 + n2 + n3 +n4);
+		if ((n2 + n3 + n4) % 2 == 0) Console.WriteLine(n2 + n3 + n4);
+		if ((n1 + n3 + n4) % 2 == 0) Console.WriteLine(n1 + n3 + n4);
+		if ((n1 + n2 + n4) % 2 == 0) Console.WriteLine(n1 + n2 + n4);
+		if ((n1 + n2 + n3) % 2 == 0) Console.WriteLine(n1 + n2 + n3);*/
+		
+		Console.WriteLine("Resultados 03");
 		if ((n1 + n2) % 2 == 0) Console.WriteLine(n1 + n2);
 		if ((n1 + n3) % 2 == 0) Console.WriteLine(n1 + n3);
 		if ((n1 + n4) % 2 == 0) Console.WriteLine(n1 + n4);
 		if ((n2 + n3) % 2 == 0) Console.WriteLine(n2 + n3);
 		if ((n2 + n4) % 2 == 0) Console.WriteLine(n2 + n4);
 		if ((n3 + n4) % 2 == 0) Console.WriteLine(n3 + n4);
+		Console.WriteLine("Resultados 04");
+		if (n1 % 2 == 0){
+			if (n2 % 2 == 0){
+				if (n3 % 2 == 0){
+					if (n4 % 2 == 0){
+						Console.WriteLine(n1 + n2 + n3 + n4);
+					}
+					else {Console.WriteLine(n1 + n2 + n3);}
+				}
+				else {Console.WriteLine(n1 + n2);}
+			}
+		}
+		else { if (n1 % 2 == 1){
+			if (n2 % 2 == 1){
+				if (n3 % 2 == 1){
+					if (n4 % 2 == 1){
+						Console.WriteLine(n1 + n2 + n3 + n4);
+					}
+					else {Console.WriteLine(n1 + n2 + n3);}
+				}
+				else {Console.WriteLine(n1 + n2);}
+				}
+			}
+		}
 	}
 }
