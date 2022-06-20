1a516c178002dc8ef88ea7eb0b55aa30a76fc8e1
Vinicius Barbosa <94053437+Vinicius-Barbosa-de-Oliveira@users.noreply.github.com>
2022-06-20 21:09:00 +0000

    metodos

--- Aula20-06/lista07/ex02.cs
+++ Aula20-06/lista07/ex02.cs
@@ -1,21 +1,22 @@
 using System;
 
 class Program {
-	public static double Maior (double x, double y, double z){
+	public static double Maior(double x, double y, double z){
 		if (x > y && x > z) { 
 			return x;
 		}
-		if (y > x && y > z) {
-			return y;
+		else {
+			if (y > x && y > z){
+				return y;
+			}
+			else return z;
 		}
-		if (z > x && z > y) {
-			return z;
-		}
+	
 	}
 	public static void Main (string[] args){
-		double x = double.Parse(Console.WriteLine());
-		double y = double.Parse(Console.WriteLine());
-		double z = double.Parse(Console.WriteLine());
+		double x = double.Parse(Console.ReadLine());
+		double y = double.Parse(Console.ReadLine());
+		double z = double.Parse(Console.ReadLine());
 
 		Console.WriteLine(Maior(x, y, z));
 	}
