adc92df8dfb9cc0d31d11732c0ccfe686ae26935
Vinicius Barbosa <94053437+Vinicius-Barbosa-de-Oliveira@users.noreply.github.com>
2022-06-22 00:34:20 +0000

    nada de interessante só adicionei o ex03 da aula 23-06

---
+++ Aula20-06/lista07/ex03.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program {
+	public static string Iniciais(string nome) {
+		string x;
+		string[] v = x.Split();
+
+		public int IndexOf(v, 1);
+
+		return x;
+		
+	}
+	public static void Main (string[] args){
+		string x = Console.ReadLine();
+
+		Console.WriteLine()
+		
+	}
+}
