9a29fdc348c9433666caf3981a94c2769e48fa4e
Vinicius Barbosa <94053437+Vinicius-Barbosa-de-Oliveira@users.noreply.github.com>
2022-07-11 13:40:40 +0000

    acrescentei algumas exercícios

--- lista-rev-03/ex11.cs
+++ lista-rev-03/ex11.cs
@@ -3,5 +3,23 @@ using System;
 class Program {
 	public static void Main (string[] args) {
 
+		Console.WriteLine("Digite a nome e o numero");
+		string v = Console.ReadLine();
+		int s1 = 0;
+
+		foreach (char d in v){
+			switch(d){
+				case '1': s1 += 1; break;
+				case '2': s1 += 2; break;
+				case '3': s1 += 3; break;
+				case '4': s1 += 4; break;
+				case '5': s1 += 5; break;
+				case '6': s1 += 6; break;
+				case '7': s1 += 7; break;
+				case '8': s1 += 8; break;
+				case '9': s1 += 9; break;	
+			}
+		}
+		Console.WriteLine(s1);
 	}
 }
--- lista-rev-03/ex15.cs
+++ lista-rev-03/ex15.cs
@@ -3,5 +3,18 @@ using System;
 class Program {
 	public static void Main (string[] args) {
 
+		Console.WriteLine("Digite a Frase:");
+		string v = Console .ReadLine();
+
+		int s2 = 0;
+		int s1 = v.IndexOf(' ');
+
+		while (s1 != -1){
+			s2 += 1;
+			v = v.Substring(s1 + 1);
+		}
+		Console.WriteLine(s2);
+		s2 = 0;
+		s1 = v.IndexOf(' ');
 	}
 }
